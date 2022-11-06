9cefc7b9b3d8cc335fee1408cf9d59e0be27648d
Kosta Kazakov <kostakazakoff@gmail.com>
2022-11-06 22:33:16 +0200

    creating a JS repo

---
+++ first_steps/PetShop.js
@@ -0,0 +1,10 @@
+function CalcFoodPrice(input) {
+    let dogFoodPrice = 2.5;
+    let catFoodPrice = 4;
+    let dogFoodPacks = Number(input[0]);
+    let catFoodPacks = Number(input[1]);
+    let total = dogFoodPacks * dogFoodPrice + catFoodPacks * catFoodPrice;
+    console.log(`${total} lv.`);
+}
+
+CalcFoodPrice("5", "4");
