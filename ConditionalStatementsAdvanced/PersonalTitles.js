39acc523026d503b72aca33b6490f8367574746f
Kosta Kazakov <kostakazakoff@gmail.com>
2022-12-13 11:50:42 +0200

    refactoring

--- ConditionalStatementsAdvanced/PersonalTitles.js
+++ ConditionalStatementsAdvanced/PersonalTitles.js
@@ -3,24 +3,49 @@ function personalTitles(input) {
     let age = Number(input[0]);
     let gender = input[1];
 
-    if (gender == 'm') {
-        if (age < 16) {
-            output = 'Master';
-        }
-        else {
-            output = 'Mr.';
-        }
-    } else if (gender == 'f') {
-        if (age < 16) {
-            output = 'Miss';
-        }
-        else {
-            output = 'Ms.';
-        }
+    switch (gender) {
+        case ('m'):
+            if (age < 16) {
+                output = 'Master';
+            } else {
+                output = 'Mr.';
+            }
+            break;
+        case ('f'):
+            if (age < 16) {
+                output = 'Miss';
+            } else {
+                output = 'Ms.';
+            }
+            break;  
     }
-
     console.log(output);
 }
+
+
+// function personalTitles(input) {
+//     let output = ''
+//     let age = Number(input[0]);
+//     let gender = input[1];
+
+//     if (gender == 'm') {
+//         if (age < 16) {
+//             output = 'Master';
+//         }
+//         else {
+//             output = 'Mr.';
+//         }
+//     } else if (gender == 'f') {
+//         if (age < 16) {
+//             output = 'Miss';
+//         }
+//         else {
+//             output = 'Ms.';
+//         }
+//     }
+
+//     console.log(output);
+// }
 
 // function personalTitles(input) {
 //     let age = Number(input[0]);
@@ -43,4 +68,4 @@ function personalTitles(input) {
 //     }
 // }
 
-// personalTitles(["12", "m"])
+personalTitles(["12", "m"])
